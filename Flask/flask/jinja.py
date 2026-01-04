f9a1d89e16b94b6a75444807d5d52d99a3c3aa94
navneetsxngh <singhnavneet2587@gmail.com>
2026-01-04 11:42:27 +0530

    Flask Files Added

--- Flask/flask/jinja.py
+++ Flask/flask/jinja.py
@@ -7,7 +7,7 @@
 {#...#} ---> THIS IS FOR COMMENTS
 '''
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -23,20 +23,6 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/form', methods = ['GET', 'POST'])
-def form():
-    if request.method == 'POST':
-        name = request.form['name']
-        return f'Hello {name}!'
-    return render_template('form.html')
-
-@app.route('/submit', methods = ['GET', 'POST'])
-def submit():
-    if request.method == 'POST':
-        name = request.form['name']
-        return f'Hello {name}!'
-    return render_template('form.html')
-
 ## Variable Rule
 @app.route('/success/<int:score>')
 def success(score):
@@ -47,17 +33,38 @@ def success(score):
         res="FAILED"
     return render_template('result.html', results = res)
 
-@app.route('/successres/<int:score>')
+@app.route('/successres/<float:score>')
 def successres(score):
     res = ""
     if score > 50:
-        res="PASSED"
+        res = "PASSED"
     else:
-        res="FAILED"
-    
-    exp = {'score' : score, "result" : res}
-    return render_template('result1.html', results = exp)
+        res = "FAILED"
+    exp = {'score': score, 'result': res}
+    return render_template('result1.html', results=exp)
 
+## If condition
+@app.route('/successif/<int:score>')
+def successif(score):
+    return render_template('result.html', results = score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return render_template('result.html', results = score)
+
+@app.route('/submit', methods = ['POST', 'GET'])
+def submit():
+    total_score = 0
+    if request.method == 'POST':
+        science = float(request.form['science'])
+        maths = float(request.form['maths'])
+        c = float(request.form['c'])
+        data_science = float(request.form['datascience'])
+
+        total_score = (science + maths + c + data_science) / 4
+    else:
+        return render_template('getresult.html')
+    return redirect(url_for('successres', score = total_score))
 
 if __name__ == '__main__':
     app.run(debug=True)
